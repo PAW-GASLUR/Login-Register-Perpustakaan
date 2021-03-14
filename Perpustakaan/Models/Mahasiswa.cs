277bc31e22d89d6bfc6c991e9605b324c200dc21
tendi darisman <50667278+ttendii@users.noreply.github.com>
2021-03-14 15:43:48 +0700

    mahasiswa

--- Perpustakaan/Models/Mahasiswa.cs
+++ Perpustakaan/Models/Mahasiswa.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace Perpustakaan.Models
-{/// <summary>
-/// Main Class
-/// </summary>
-/// <remarks>
-/// Class Mahasiswa digunakan untuk verifikasi data mahasiswa yang akan dimasukan
-/// </remarks>
+{
+    /// <summary>
+    /// Main class 
+    /// </summary>
+    /// <remarks>
+    /// Class Mahasiswa digunakan untuk verifikasi data mahasiswa yang akan ditambahkan
+    /// </remarks>
     public partial class Mahasiswa
     {
         
+      
         /// <summary>
-        /// Class mahasiswa
+        /// Class Mahasiswa
         /// </summary>
         public Mahasiswa()
         {
@@ -21,32 +23,38 @@ namespace Perpustakaan.Models
         }
 
 
+        
         /// <summary>
-        /// untuk verifikasi data no anggota mahasiswa
+        /// Untuk verifikasi nomor anggota
         /// </summary>
         public int NoAnggota { get; set; }
+       
         /// <summary>
-        /// untuk verifikasi nomor induk mahasiswa
+        /// Untuk verifikasi nomor induk mahasiswa
         /// </summary>
         [Required(ErrorMessage = "NIM tidak boleh kosong!!")]
         public string Nim { get; set; }
+       
         /// <summary>
         /// Untuk verifikasi nama mahasiswa
         /// </summary>
         [Required(ErrorMessage = "Nama tidak boleh kosong!!")]
         public string Nama { get; set; }
+      
         /// <summary>
-        /// Untuk verifikasi no gender mahasiswa
+        /// Untuk verifikasi nomor gender mahasiswa
         /// </summary>
         [Required(ErrorMessage = "No Gender tidak boleh kosong!!")]
         public int? NoGender { get; set; }
+       
         /// <summary>
-        /// Untuk verifikasi no HP mahasiswa
+        /// Untuk verifikasi nomor hp mahasiswa
         /// </summary>
         [Required(ErrorMessage = "No Hp tidak boleh kosong!!")]
         public string NoHp { get; set; }
+       
         /// <summary>
-        /// Untuk verifikasi Alamat mahasiswa
+        /// Untuk verifikasi alamat mahasiswa
         /// </summary>
         [Required(ErrorMessage = "Alamat tidak boleh kosong!!")]
         public string Alamat { get; set; }
